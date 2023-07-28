cf5997054c43fe289a0348c4908acd7515d29c62
Dario Voll <dariovoll@gmail.com>
2023-07-28 13:19:45 +0200

    Initial commit

---
+++ hashtest.js
@@ -0,0 +1,75 @@
+/**
+ * Returns a hash code from a string
+ * @param  {String} str The string to hash.
+ * @return {Number}    A 32bit integer
+ * @see http://werxltd.com/wp/2010/05/13/javascript-implementation-of-javas-string-hashcode-method/
+ */
+function hashCode(str) {
+    let hash = 0;
+    for (let i = 0, len = str.length; i < len; i++) {
+        let chr = str.charCodeAt(i);
+        hash = (hash << 5) - hash + chr;
+        hash |= 0; // Convert to 32bit integer
+    }
+    return hash;
+}
+
+function base64_encode (s)
+{
+  // the result/encoded string, the padding string, and the pad count
+  var base64chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+  var r = ""; 
+  var p = ""; 
+  var c = s.length % 3;
+
+  // add a right zero pad to make this string a multiple of 3 characters
+  if (c > 0) { 
+    for (; c < 3; c++) { 
+      p += '='; 
+      s += "\0"; 
+    } 
+  }
+
+  // increment over the length of the string, three characters at a time
+  for (c = 0; c < s.length; c += 3) {
+
+    // we add newlines after every 76 output characters, according to the MIME specs
+    if (c > 0 && (c / 3 * 4) % 76 == 0) { 
+      r += "\r\n"; 
+    }
+
+    // these three 8-bit (ASCII) characters become one 24-bit number
+    var n = (s.charCodeAt(c) << 16) + (s.charCodeAt(c+1) << 8) + s.charCodeAt(c+2);
+
+    // this 24-bit number gets separated into four 6-bit numbers
+    n = [(n >>> 18) & 63, (n >>> 12) & 63, (n >>> 6) & 63, n & 63];
+
+    // those four 6-bit numbers are used as indices into the base64 character list
+    r += base64chars[n[0]] + base64chars[n[1]] + base64chars[n[2]] + base64chars[n[3]];
+  }
+   // add the actual padding string, after removing the zero pad
+  return r.substring(0, r.length - p.length) + p;
+}
+
+console.log(base64_encode('400 2AAA A400 0552 0002'));
+
+
+// Beispiel-Matrix (5x5)
+const matrix = [
+    [0, 0, 0, 4, 5],
+    [6, 7, 5, 5, 1],
+    [2, 3, 4, 5, 6],
+    [7, 8, 8, 1, 2],
+    [3, 4, 5, 6, 7],
+  ];
+  
+  // Matrix in eine Zeichenkette umwandeln
+  const matrixString = matrix.flat().join('');
+
+  console.log('matrixString', matrixString);
+  // Zeichenkette in Base36 umwandeln
+  const decimal = BigInt(parseInt(matrixString, 10))
+  console.log('decimal', decimal);
+  const base36String = decimal.toString(36);
+  
+  console.log('base36String', base36String);
---
+++ src/game-grid/game-grid.ts
@@ -0,0 +1,84 @@
+export enum FieldType {
+    EMPTY_EMPTY = 0, // 7
+    EMPTY_TARGET = 1, // 5
+    TARGET_EMPTY = 2, // 5
+    TARGET_TARGET = 3, // 3
+    TARGET_BOMB = 4, // 1
+    BOMB_TARGET = 5, // 1
+    EMPTY_BOMB = 6, // 1
+    BOMB_EMPTY = 7, // 1
+    BOMB_BOMB = 8, // 1
+}
+
+export class GameGrid {
+
+    // matrix: FieldType[][];
+    // this.matrix = Array.from(Array(5), () => new Array(5));
+    private list: FieldType[];
+
+    static frequencyMapping: [type: FieldType, frequency: number][] = [
+        [FieldType.EMPTY_EMPTY, 7],
+        [FieldType.EMPTY_TARGET, 5],
+        [FieldType.TARGET_EMPTY, 5],
+        [FieldType.TARGET_TARGET, 3],
+        [FieldType.TARGET_BOMB, 1],
+        [FieldType.BOMB_TARGET, 1],
+        [FieldType.EMPTY_BOMB, 1],
+        [FieldType.BOMB_EMPTY, 1],
+        [FieldType.BOMB_BOMB, 1],
+    ];
+
+    constructor(code?: string) {
+        this.list = new Array(25).fill(undefined);
+
+    }
+
+    getField(x: number = 0, yLine?: number) {
+        if (yLine !== undefined) {
+            const index = x * yLine;
+            if (index >= this.list.length) {
+                throw new Error('No grid field for position');
+            }
+            return this.list[index];
+        }
+        return this.list[x];
+    }
+
+    getAllFields() {
+        return [...this.list];
+    }
+
+    getCode() {
+        const decimal = BigInt(parseInt(this.list.join(''), 10))
+        console.log('decimal', decimal);
+        const base36String = decimal.toString(36);
+        console.log('base36String', base36String);
+        return base36String;
+    }
+
+    generate() {
+        this.list = new Array(25).fill(undefined);
+        GameGrid.frequencyMapping.forEach(map => {
+            for (let i = 0; i < map[1]; i++) {
+                this.setListField(map[0]);
+            } 
+        });
+    }
+
+    private getFromCode() {
+        
+    }
+
+    private setListField(fieldType: FieldType) {
+        const realEmptyFields = this.list.reduce<number[]>((prev, curr, currIndex) => {
+            return curr === undefined ? prev.concat([currIndex]) : prev
+        }, []);
+        const realEmptyFieldIndex = realEmptyFields[this.getRandomNumber(realEmptyFields.length - 1)]
+        this.list[realEmptyFieldIndex] = fieldType;
+        
+    }
+    
+    private getRandomNumber(max = 10) {
+        return Math.round(Math.random() * max);
+    }
+}
