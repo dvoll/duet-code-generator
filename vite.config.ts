5a0e2b06e5f5f207cf3459e474d99f87b9483f4f
Dario Voll <dvoll@uandi.com>
2023-08-07 23:29:41 +0200

    Fix vite build base

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    base: '/duet-code-generator/',
     test: {
         globals: true,
         environment: 'jsdom',
